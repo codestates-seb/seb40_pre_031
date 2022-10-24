c746a23c47425922cdd193d3ee233ab1447d330e
toneofrain <saintho95@gmail.com>
2022-10-24 18:42:05 +0900

    feat: server initial setting

---
+++ server/src/main/java/com/codestates/Pre031Application.java
@@ -0,0 +1,13 @@
+package com.codestates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pre031Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pre031Application.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/codestates/Pre031ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pre031ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
