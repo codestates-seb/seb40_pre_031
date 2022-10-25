83d81973b9d9f70fd6b6fd41ddaa1622546aa288
kexxxon <dev.kexon@gmail.com>
2022-10-26 04:02:29 +0900

    ✨feat: Auditing 구현 - Auditing을 상속받는 모든 객체에 생성시간 / 수정시간이 자동으로 관리됨

--- server/src/main/java/com/codestates/Pre031Application.java
+++ server/src/main/java/com/codestates/Pre031Application.java
@@ -2,8 +2,10 @@ package com.codestates;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class Pre031Application {
 
 	public static void main(String[] args) {
