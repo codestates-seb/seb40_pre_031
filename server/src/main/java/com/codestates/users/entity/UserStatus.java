0a75c73a3f409475abb8e6428ae80c61c9273ec4
Bhinney <snakellie97@gmail.com>
2022-10-26 11:42:00 +0900

    feat: User Entity Class 구현

---
+++ server/src/main/java/com/codestates/users/entity/UserStatus.java
@@ -0,0 +1,16 @@
+package com.codestates.users.entity;
+
+import lombok.Getter;
+
+public enum UserStatus {
+	USER_ACTIVE("회원 활동 중"),
+	USER_DORMANT("회원 휴면"),
+	USER_OUT("회원 탈퇴");
+
+	@Getter
+	private String status;
+
+	UserStatus(String status) {
+		this.status = status;
+	}
+}
