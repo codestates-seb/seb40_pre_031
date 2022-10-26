33c415fe192f97486bec4a5069f3de2da5dc6711
kexxxon <dev.kexon@gmail.com>
2022-10-26 16:12:13 +0900

    ✨feat: QuestionVote 구현 - 추천/상태없음/비추천 상태 관리 가능

---
+++ server/src/main/java/com/codestates/question/entity/QuestionVote.java
@@ -0,0 +1,34 @@
+package com.codestates.question.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class QuestionVote {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "question_vote_id")
+	private Long id;
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false, length = 20)
+	private QuestionVoteStatus questionVoteStatus = QuestionVoteStatus.NONE;
+
+	// mapping 필요
+	// private User user;
+
+	// mapping 필요
+	// private Question question;
+}
