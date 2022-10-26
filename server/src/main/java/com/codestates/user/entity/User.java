ea8e90879ad5359389b615393e896abdf0682836
Bhinney <107696895+Bhinney@users.noreply.github.com>
2022-10-26 16:23:29 +0900

    ✨feat: User Entity Class 구현 (#25)
    * feat: User Entity Class 구현
    
    * fix: 컨밴션 수정
    
    * fix: package 이름 변경

---
+++ server/src/main/java/com/codestates/user/entity/User.java
@@ -0,0 +1,39 @@
+package com.codestates.user.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity(name = "USERS")
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "USER_ID")
+	private Long id;
+	@Column(nullable = false, updatable = false, unique = true)
+	private String email;
+	@Column(length = 30, nullable = false)
+	private String password;
+	@Column(length = 50, nullable = false)
+	private String displayName;
+	@Enumerated(EnumType.STRING)
+	@Column(length = 20, nullable = false)
+	private UserStatus usersStatus = UserStatus.USER_ACTIVE;
+
+	public User(String email, String password, String displayName) {
+		this.email = email;
+		this.password = password;
+		this.displayName = displayName;
+	}
+}
