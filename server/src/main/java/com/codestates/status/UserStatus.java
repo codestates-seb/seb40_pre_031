e5edcacf36e89edf1a1c07baa1204ccedba26450
toneofrain <saintho95@gmail.com>
2022-10-27 15:48:55 +0900

    feat: UserStatus enum 클래스에 ADMIN 추가 (#34)
    관리자 분류를 위해 ADMIN을 추가하였습니다.

--- server/src/main/java/com/codestates/status/UserStatus.java
+++ server/src/main/java/com/codestates/status/UserStatus.java
@@ -5,10 +5,11 @@ import lombok.Getter;
 public enum UserStatus {
 	USER_ACTIVE("회원 활동 중"),
 	USER_DORMANT("회원 휴면"),
-	USER_OUT("회원 탈퇴");
+	USER_OUT("회원 탈퇴"),
+	ADMIN("관리자 계정");
 
 	@Getter
-	private String status;
+	private final String status;
 
 	UserStatus(String status) {
 		this.status = status;
