afdeb05b118408583763beda81d80612abc398e5
waymokorea <taw0207@naver.com>
2022-10-28 11:47:03 +0900

    ✨feat : LeftNav 리스트와 헤드 추가

--- client/src/components/atoms/LeftNavHead.js
+++ client/src/components/atoms/LeftNavHead.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
 
-function LeftNavHead() {
-  return <></>;
+const Itemhead = styled.li`
+  box-sizing: border-box;
+  display: flex;
+  justify-content: left;
+  align-items: center;
+  width: 156px;
+  height: 14.38px;
+  font-size: 11px;
+  margin: 16px 0 4px 8px;
+  color: #6a737c;
+`;
+
+// 왼쪽사이드바의 목록 제목 부분
+function LeftNavHead({ name }) {
+  return <Itemhead>{name}</Itemhead>;
 }
 
 export default LeftNavHead;
