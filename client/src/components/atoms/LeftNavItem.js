fdc21fef65e8bb1737d4e8e8d1d47655933632a2
waymokorea <taw0207@naver.com>
2022-10-28 11:43:57 +0900

    ✨feat : Navlink atom 생성

--- client/src/components/atoms/LeftNavItem.js
+++ client/src/components/atoms/LeftNavItem.js
@@ -1,7 +1,36 @@
 import React from 'react';
+import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
 
-function LeftNavItem() {
-  return <></>;
+const StyledLink = styled(NavLink)`
+  box-sizing: border-box;
+  display: flex;
+  justify-content: left;
+  align-items: center;
+  width: 164px;
+  height: 100%;
+  padding: ${(props) => props.padding || '4px 4px 4px 30px'};
+  text-decoration: none;
+  color: #525960;
+  cursor: pointer;
+  :hover {
+    color: #0c0d0e;
+  }
+  &.active {
+    background: #f1f2f3;
+    color: #0c0d0e;
+    border-right: 5px solid #f48224;
+    font-weight: bold;
+  }
+`;
+
+// 왼쪽 사이드바의 링크 부분 , 라우터의 Navlink를 이용한 페이지 이동
+function LeftNavItem({ padding, link, name }) {
+  return (
+    <StyledLink padding={padding} end to={link}>
+      <span>{name}</span>
+    </StyledLink>
+  );
 }
 
 export default LeftNavItem;
