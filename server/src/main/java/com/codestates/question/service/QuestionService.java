821d44681eafe63fb3a024104388a732445b7f47
kexxxon <dev.kexon@gmail.com>
2022-10-28 22:26:06 +0900

    feat: QuestionService 기본 구조 구현

---
+++ server/src/main/java/com/codestates/question/service/QuestionService.java
@@ -0,0 +1,44 @@
+package com.codestates.question.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.codestates.question.entity.Question;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class QuestionService {
+	/* DI */
+
+	/* 질문글 등록 */
+	public Question createQuestion() {
+
+		return null;
+	}
+
+	/* 질문글 수정 */
+	public Question updateQuestion() {
+
+		return null;
+	}
+
+	/* 특정 질문글 조회 */
+	public Question findQuestion() {
+
+		return null;
+	}
+
+	/* 전체 질문글 조회 */
+	public List<Question> findQuestions() {
+
+		return null;
+	}
+
+	/* 질문글 삭제 */
+	public void deleteQuestion() {
+
+	}
+}
