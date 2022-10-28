4950b38fd640c28fec4955172e7e0da41022cfdb
waymokorea <taw0207@naver.com>
2022-10-28 11:41:12 +0900

    ✨feat : Leftnav 레이아웃

--- client/src/components/organism/LeftNav.js
+++ client/src/components/organism/LeftNav.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
+import LeftNavList from '../molecules/LeftNavList';
 
-function LeftNav() {
-  return <></>;
+const Container = styled.div`
+  position: sticky;
+  top: 0px;
+  padding-top: 24px;
+  display: flex;
+  align-items: center;
+  width: 164px;
+  height: 100%;
+  background: #ffffff;
+  z-index: 10;
+`;
+// 왼쪽 사이드바를 재활용하기 위한 Organism
+
+function Leftnav() {
+  return (
+    <Container>
+      <LeftNavList></LeftNavList>
+    </Container>
+  );
 }
 
-export default LeftNav;
+export default Leftnav;
