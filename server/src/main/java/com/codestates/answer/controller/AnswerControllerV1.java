204da89f8e91263e1ff4b0538a1089fefbcef1c6
toneofrain <toneofrain@gmail.com>
2022-10-29 21:21:35 +0900

    feat: 답변 수정, 삭제 api 구현 (#67)

--- server/src/main/java/com/codestates/answer/controller/AnswerControllerV1.java
+++ server/src/main/java/com/codestates/answer/controller/AnswerControllerV1.java
@@ -1,21 +1,23 @@
 package com.codestates.answer.controller;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Positive;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.codestates.answer.dto.AnswerPatchDto;
 import com.codestates.answer.dto.AnswerPostDto;
 import com.codestates.answer.entity.Answer;
 import com.codestates.answer.service.AnswerService;
-import com.codestates.question.entity.Question;
-import com.codestates.user.entity.User;
 
 import groovy.util.logging.Slf4j;
 import lombok.RequiredArgsConstructor;
@@ -25,25 +27,32 @@ import lombok.RequiredArgsConstructor;
 @Validated
 @RequiredArgsConstructor
 @Slf4j
-public class AnswerController {
+public class AnswerControllerV1 {
 	private final AnswerService answerService;
 
 	@PostMapping("")
-	public ResponseEntity postAnswer(@PathVariable(name = "question_id") Long questionId,
+	public ResponseEntity postAnswer(@Positive @PathVariable(name = "question_id") Long questionId,
 		@Valid @RequestBody AnswerPostDto answerPostDto) {
-		User user = new User("saintho95@gmail.com", "abcd1234%^&*", "성호");
-		user.setId(answerPostDto.getUserId());
-
-		Question question = new Question();
-		question.setId(questionId);
-
 		Answer answer = new Answer();
 		answer.setContent(answerPostDto.getContent());
-		// answer.setUser(user);
-		// answer.setQuestion(question);
 
 		answerService.createAnswer(answer);
 
 		return ResponseEntity.status(HttpStatus.CREATED).build();
+	}
+
+	@PatchMapping("{answer_id}")
+	public ResponseEntity patchAnswer(@Positive @PathVariable(name = "answer_id") Long answerId,
+		@Valid @RequestBody AnswerPatchDto answerPatchDto) {
+		answerService.updateAnswer(answerId, answerPatchDto.getContent());
+
+		return ResponseEntity.ok().build();
+	}
+
+	@DeleteMapping("{answer_id}")
+	public ResponseEntity deleteAnswer(@Positive @PathVariable(name = "answer_id") Long answerId) {
+		answerService.deleteAnswer(answerId);
+
+		return ResponseEntity.noContent().build();
 	}
 }
--- server/src/main/java/com/codestates/answer/service/AnswerService.java
+++ server/src/main/java/com/codestates/answer/service/AnswerService.java
@@ -1,5 +1,7 @@
 package com.codestates.answer.service;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
 import com.codestates.answer.entity.Answer;
@@ -12,7 +14,27 @@ import lombok.RequiredArgsConstructor;
 public class AnswerService {
 	private final AnswerRepository answerRepository;
 
-	public Answer createAnswer(Answer answer) {
-		return answerRepository.save(answer);
+	public void createAnswer(Answer answer) {
+		answerRepository.save(answer);
+	}
+
+	public void updateAnswer(Long answerId, String content) {
+		Answer found = findVerifiedAnswer(answerId);
+		found.setContent(content);
+
+		answerRepository.save(found);
+	}
+
+	public void deleteAnswer(Long answerId) {
+		Answer found = findVerifiedAnswer(answerId);
+
+		answerRepository.delete(found);
+	}
+
+	public Answer findVerifiedAnswer(Long answerId) {
+		Optional<Answer> found = answerRepository.findById(answerId);
+
+		return found.orElseThrow(() ->
+			new RuntimeException("ANSWER_NOT_FOUND"));
 	}
 }
