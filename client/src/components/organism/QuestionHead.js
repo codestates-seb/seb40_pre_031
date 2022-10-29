5c7dfc63e7dcf0cb497ba183acd3ddeba46bb0a1
dorrion <dhdms8696@naver.com>
2022-10-29 17:31:29 +0900

    🎨style : 질문 목록 상단 스타일 추가

--- client/src/components/organism/QuestionHead.js
+++ client/src/components/organism/QuestionHead.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
 import QuestionRow from '../molecules/QuestionRow';
 import QuestionSortRow from '../molecules/QuestionSortRow';
 
+const QuestionHeadLayout = styled.div`
+  padding: 12px;
+  border-bottom: 1px solid var(--bc-light);
+  `;
 
 const QuestionHead = () => {
   return (
-    <div>
+    <QuestionHeadLayout>
       <QuestionRow />
       <QuestionSortRow />
-    </div>
+    </QuestionHeadLayout>
   );
 };
 
