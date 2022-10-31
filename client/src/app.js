2d977d7dd95ffd8e6b45af27d737a6b242728851
Oheun Kim <dhdms8696@naver.com>
2022-10-31 23:42:12 +0900

    Create app.js

---
+++ client/src/app.js
@@ -0,0 +1,16 @@
+import { ThemeProvider } from "styled-components";
+import theme from "./theme";
+import GlobalStyle from "./GlobalStyles";
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        
+      </ThemeProvider>
+    </>
+  );
+}
+
+export default App;
