063c3cc746be27377de8eb648527961211e52b1e
dorrion <dhdms8696@naver.com>
2022-11-01 23:59:05 +0900

    🎨style : molecules ESLint와 prettier 적용

--- client/src/components/molecules/QuestionSummaryContent.js
+++ client/src/components/molecules/QuestionSummaryContent.js
@@ -1,12 +1,11 @@
-import React from "react";
-import styled from "styled-components";
-import QuestionTitle from "../atoms/QuestionTitle";
-import QuestionUser from "./QuestionUser";
+import styled from 'styled-components';
+import QuestionTitle from '../atoms/QuestionTitle';
+import QuestionUser from './QuestionUser';
 
 const QuestionSummaryContenLayout = styled.div`
   display: flex;
   flex-direction: column;
-  `;
+`;
 
 const QuestionContent = styled.div`
   display: flex;
@@ -24,12 +23,11 @@ const QuestionSummaryContent = () => {
       <QuestionTitle />
       <QuestionContent>
         I have a p-dialog modal with Date fields (using p-calendar). When I
-        enter dates into them manually and press 'Tab' key, the dates get
-        erased. Please let me
+        enter dates into them manually and press Tab key, the dates get erased.
+        Please let me
       </QuestionContent>
       <QuestionUser />
     </QuestionSummaryContenLayout>
-    
   );
 };
 
