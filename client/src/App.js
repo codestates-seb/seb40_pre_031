35f0806a0e0254eb521686e91e0c6c427da734f3
taewook <100844959+waymokorea@users.noreply.github.com>
2022-11-01 15:25:14 +0900

    Feat/53-fe- 질문 작성 페이지  (#92)
    * ✨feat : 기본 레이아웃 구현
    
    * ✨feat : 에디터 기능 레이아웃
    
    * ✨feat : 제목 글자수 35자 경고 추가
    
    * ✨feat : axios post
    
    * ✨feat : 수정 및 주석 작성
    
    * 🚑fix : Eslint 수정

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,29 @@
+<<<<<<< HEAD
+import { ThemeProvider } from 'styled-components';
+import theme from './theme';
+import GlobalStyle from './GlobalStyles';
+=======
 import { ThemeProvider } from "styled-components";
 import theme from "./theme";
 import GlobalStyle from "./GlobalStyles";
+>>>>>>> dev-fe
 
 function App() {
   return (
     <>
       <GlobalStyle />
+<<<<<<< HEAD
+      <ThemeProvider theme={theme}></ThemeProvider>
+    </>
+  );
+}
+
+=======
       <ThemeProvider theme={theme}>
         
       </ThemeProvider>
     </>
   );
 }
+>>>>>>> dev-fe
 export default App;
---
+++ client/src/components/molecules/AskQuestionEditorBox.js
@@ -0,0 +1,36 @@
+import { forwardRef } from 'react';
+import styled from 'styled-components';
+import Write from './Editor';
+
+// 에디터를 포함하는 부분
+const Editorbox = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  gap: 5px;
+  h3 {
+    font-size: 15px;
+    color: var(--black-900);
+  }
+  p {
+    font-size: 12px;
+    color: var(--fc-medium);
+  }
+`;
+
+const AskQuestionEditorBox = forwardRef(({ onChange }, ref) => {
+  return (
+    <Editorbox>
+      <h3>Body</h3>
+      <p>
+        Include all the information someone would need to answer your question
+      </p>
+      <Write ref={ref} onChange={onChange} />
+    </Editorbox>
+  );
+});
+
+AskQuestionEditorBox.displayName = 'AskQuestionEditorBox';
+
+export default AskQuestionEditorBox;
