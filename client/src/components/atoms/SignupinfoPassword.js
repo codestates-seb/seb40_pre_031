cfbb2a07a4e682f7758a074a0351781ae3956ca2
dorrion <dhdms8696@naver.com>
2022-11-01 23:53:52 +0900

    🎨style : ESLint와 prettier 적용
    - 사용하지 않은 React 삭제
    - 문서서식 적용

--- client/src/components/atoms/SignupinfoPassword.js
+++ client/src/components/atoms/SignupinfoPassword.js
@@ -1,18 +1,18 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 //유효성 검사시 나타나는 메세지
 const SignupinfoPassword = styled.p`
-    width : 268px;
-    margin : ${({margin,theme}) => theme.margin[margin]};
-    font-size : 12px;
-    font-weight : 500;
-    color : ${({color}) => color}; 
-`
+  width: 268px;
+  margin: ${({ margin, theme }) => theme.margin[margin]};
+  font-size: 12px;
+  font-weight: 500;
+  color: ${({ color }) => color};
+`;
 const SignupinfoExplamation = styled.p`
-width : 268px;
-height: 20px;
-font-size : 12px;
-font-weight : 500;
-color : rgb(229	62	66);
-`
-export  {SignupinfoPassword, SignupinfoExplamation};
+  width: 268px;
+  height: 20px;
+  font-size: 12px;
+  font-weight: 500;
+  color: rgb(229 62 66);
+`;
+export { SignupinfoPassword, SignupinfoExplamation };
