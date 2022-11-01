27695c5c91de2e7dcc643d09af9c1fc0ba4e5eec
Oheun Kim <dhdms8696@naver.com>
2022-11-01 22:19:44 +0900

    Revert "✨feat : Header 구현 (#97)" (#104)
    This reverts commit 79f3eb3b1cae35d65d4bfeda93c1ab0843e4700c.

---
+++ client/src/components/molecules/AskQuestionTagBox.js
@@ -0,0 +1,43 @@
+import { forwardRef } from 'react';
+import styled from 'styled-components';
+
+// 질문 tag 부분을 묶는 div
+
+const TagBox = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+  h3 {
+    font-size: 15px;
+    color: var(--black-900);
+  }
+  p {
+    font-size: 12px;
+    color: var(--fc-medium);
+  }
+  input {
+    width: 100%;
+    padding: 7.8px 9.1px;
+    border: 1px solid var(--black-200);
+    border-radius: 3px;
+  }
+`;
+
+// eslint-disable-next-line no-empty-pattern
+const AskQuestionTagBox = forwardRef(({}, ref) => {
+  return (
+    <TagBox>
+      <h3>Tags</h3>
+      <p>
+        Add up to 5 tags to describe what your question is about. Start typing
+        to see suggestions
+      </p>
+      <input type="text" placeholder="e,g (arrays xml json)" ref={ref}></input>
+    </TagBox>
+  );
+});
+
+AskQuestionTagBox.displayName = 'AskQuestionTagBox';
+
+export default AskQuestionTagBox;
---
+++ client/src/components/organism/QuestionList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
+import QuestionSummary from '../molecules/QuestionSummary';
+import QuestionSummaryContent from '../molecules/QuestionSummaryContent';
+
+const QuestionListLayout = styled.div`
+  display: flex;
+  padding: 12px;
+  border-bottom: 1px solid var(--bc-light);
+  `
+
+const QuestionList = () => {
+  return (
+    <QuestionListLayout>
+      <QuestionSummary />
+      <QuestionSummaryContent />
+    </QuestionListLayout>
+  );
+};
+
+export default QuestionList;
