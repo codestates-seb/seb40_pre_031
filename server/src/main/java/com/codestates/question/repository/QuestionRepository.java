481b2926c9a70fa4462236300d34723b91941236
kexxxon <dev.kexon@gmail.com>
2022-11-03 04:32:50 +0900

    feat: QuestionRepository 구현

--- server/src/main/java/com/codestates/question/repository/QuestionRepository.java
+++ server/src/main/java/com/codestates/question/repository/QuestionRepository.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -14,8 +15,10 @@ import com.codestates.question.entity.Question;
 public interface QuestionRepository extends JpaRepository<Question, Long> {
 	Optional<Question> findById(Long userId);
 
-	/* pagination */
-	/* 생성시간으로 정렬 */
 	@Query("select question from Question question order by question.createdAt desc")
 	Page<Question> questionPage(Pageable pageable);
+
+	@Modifying
+	@Query("update Question q set q.views = q.views + 1 where q.id = :id")
+	int updateView(Long id);
 }
