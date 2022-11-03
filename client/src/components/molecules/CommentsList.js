64c817dc5b4b9c2c128f1144f4d015ae447d6b1d
waymokorea <taw0207@naver.com>
2022-11-03 20:23:14 +0900

    ✨feat : 데이터 시간 차이 추가

--- client/src/components/molecules/CommentsList.js
+++ client/src/components/molecules/CommentsList.js
@@ -4,14 +4,42 @@ const CommentsBox = styled.div`
   box-sizing: border-box;
   margin-top: 5px;
   margin-bottom: 5px;
-  border-top: 1px solid var(--black-075);
-  border-bottom: 1px solid var(--black-075);
 `;
 
-function CommentsList() {
+function CommentsList({ data }) {
+  console.log(data);
+  const TimeCheck = (time) => {
+    const commentTime = new Date(Date.parse(time));
+    const nowTime = Date.now();
+
+    const differenceTime = nowTime - commentTime.getTime();
+    const differenceDays = Math.floor(differenceTime / (1000 * 60 * 60 * 24));
+    const differencehours = Math.floor(differenceTime / (60 * 60 * 1000));
+    const differenceminutes = Math.floor(differenceTime / (60 * 1000));
+    const differenceseconds = Math.floor(differenceTime / 1000);
+
+    if (differenceDays > 30) {
+      return commentTime.toLocaleString();
+    } else if (differencehours > 24) {
+      return differenceDays + ' days ago';
+    } else if (differenceminutes > 60) {
+      return differencehours + ' hours ago';
+    } else if (differenceseconds > 60) {
+      return differenceminutes + ' mins ago';
+    } else {
+      return differenceseconds + ' seconds ago';
+    }
+  };
   return (
     <CommentsBox>
-      <Comment />
+      {data.map((comment) => (
+        <Comment
+          key={comment.comments_id}
+          content={comment.content}
+          id={comment.user_id}
+          time={TimeCheck(comment.modified_at)}
+        />
+      ))}
     </CommentsBox>
   );
 }
