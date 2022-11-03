95cebfff4ae4d2c83fd8fad22456207122e138fa
JY <107452870+GitHubJIYEON@users.noreply.github.com>
2022-11-03 18:42:18 +0900

    ✨feat : RightSidebar 구현 (#137)
    * RightSidebarA(yellowBox)
    
    * RightSidebarA style적용
    
    * 오른쪽 사이드바 구현
    
    * 오른쪽 노란색 사이드바 구현
    
    * Svg파일
    
    * globalStyles적용
    
    * 노란색오른쪽사이드바 구현
    
    * 오른쪽 두번째 사이드바 헤더
    
    * 오른쪽사이드바 구현
    
    * 오른쪽 사이드바 구현
    
    * RightSidebar Temlates

---
+++ client/src/components/templates/RightSidebar.js
@@ -0,0 +1,15 @@
+import RightSidebarA from '../organism/RightSidebarA';
+import RightSidebarTittleBox from '../atoms/RightSidebarTittleBox';
+import RightSidebarBox from '../molecules/RightSidebarBox';
+
+const RightSidebar = () => {
+  return (
+    <>
+      <RightSidebarA />
+      <RightSidebarTittleBox />
+      <RightSidebarBox />
+    </>
+  );
+};
+
+export default RightSidebar;
