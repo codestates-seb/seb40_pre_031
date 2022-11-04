95dc1ebce699cdea978cf67df452492204c1a615
kexxxon <dev.kexon@gmail.com>
2022-11-04 21:35:45 +0900

    feat: QuestionService 수정 및 추가 구현

--- server/src/main/java/com/codestates/question/service/QuestionService.java
+++ server/src/main/java/com/codestates/question/service/QuestionService.java
@@ -2,11 +2,11 @@ package com.codestates.question.service;
 
 import java.util.Optional;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.codestates.answer.entity.Answer;
+import com.codestates.answer.repository.AnswerRepository;
 import com.codestates.question.entity.Question;
 import com.codestates.question.entity.QuestionVote;
 import com.codestates.question.repository.QuestionRepository;
@@ -19,9 +19,11 @@ import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class QuestionService {
 	private final QuestionVoteRepository questionVoteRepository;
 	private final QuestionRepository questionRepository;
+	private final AnswerRepository answerRepository;
 	private final UserRepository userRepository;
 
 	public Question createQuestion(Question question) {
@@ -39,14 +41,10 @@ public class QuestionService {
 		return questionRepository.save(verifiedQuestion);
 	}
 
+	@Transactional(readOnly = true)
 	public Question findQuestion(Long questionId) {
 
 		return findVerifiedQuestion(questionId);
-	}
-
-	public Page<Question> findQuestions(int page, int size) {
-
-		return questionRepository.questionPage(PageRequest.of(page, size));
 	}
 
 	public void deleteQuestion(Long questionId) {
@@ -55,7 +53,8 @@ public class QuestionService {
 		questionRepository.deleteById(questionId);
 	}
 
-	private Question findVerifiedQuestion(Long questionId) {
+	@Transactional(readOnly = true)
+	public Question findVerifiedQuestion(Long questionId) {
 		Optional<Question> getQuestion = questionRepository.findById(questionId);
 
 		return getQuestion.orElseThrow(
@@ -73,9 +72,19 @@ public class QuestionService {
 		return optionalQuestionVote.get().getStatus();
 	}
 
-	@Transactional
 	public int updateView(Long id) {
 
 		return this.questionRepository.updateView(id);
+	}
+
+	public void chosenAnswer(Long questionId, Long chosenAnswerId) {
+		Answer answer = answerRepository.findById(chosenAnswerId)
+			.orElseThrow(() -> new RuntimeException("ANSWER_NOT_FOUND"));
+
+		if (!answer.getQuestion().getId().equals(questionId)) {
+			throw new RuntimeException("WRONG_ID");
+		}
+
+		answer.getQuestion().setChosenAnswerId(chosenAnswerId);
 	}
 }
