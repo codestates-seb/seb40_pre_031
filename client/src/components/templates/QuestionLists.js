5100c3fcec71801f7e106cfecd1e03ee65f80b59
dorrion <dhdms8696@naver.com>
2022-11-04 15:04:46 +0900

    ✨feat : 데이터 불러와서 내리기

--- client/src/components/templates/QuestionLists.js
+++ client/src/components/templates/QuestionLists.js
@@ -1,11 +1,41 @@
+/* eslint-disable no-unused-vars */
+import { useEffect, useState } from 'react';
+import { questionApi } from '../../api/apis';
 import QuestionList from '../organism/QuestionList';
+import axios from 'axios';
 
 const QuestionLists = () => {
-  return (
-    <div>
-      <QuestionList />
-    </div>
-  );
+  const [Questions, setQuestions] = useState([]); // 백엔드 통신으로 데이터 받아오기
+  const [loading, setloading] = useState(false);
+  const [currentPage, setcurrentPage] = useState(1); // 초기 값 1
+  const [questionPerPage, setquestionPerPage] = useState(5); // 한 페이지에 보여줄 질문 개수
+
+  useEffect(() => {
+    questionApi
+      .getQuestion()
+      .then((res) => {
+        setQuestions(res.data);
+        console.log(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  //Get Current Products
+  // const indexOfLastQuestion = Questions * questionPerPage;
+  // const indexOfFirstQuestion = indexOfLastQuestion - questionPerPage;
+  // const currentQuestion = Questions.slice(
+  //   indexOfFirstQuestion,
+  //   indexOfLastQuestion
+  // );
+
+  // const questionList = currentQuestion.map((question, index) => {
+  //   console.log(question);
+  //   return <QuestionList key={index} question={question} />;
+  // });
+
+  return Questions.map((question, index) => {
+    return <QuestionList key={index} question={question} />;
+  });
 };
 
 export default QuestionLists;
