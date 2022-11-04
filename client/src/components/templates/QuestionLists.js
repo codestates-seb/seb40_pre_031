9fe81a23fa1a040306e19aad07ea2e04ef3d2e2f
dorrion <dhdms8696@naver.com>
2022-11-04 16:22:43 +0900

    ✨feat : 페이지네이션 커스텀 훅

--- client/src/components/templates/QuestionLists.js
+++ client/src/components/templates/QuestionLists.js
@@ -1,14 +1,14 @@
 /* eslint-disable no-unused-vars */
 import { useEffect, useState } from 'react';
+import styled from 'styled-components';
 import { questionApi } from '../../api/apis';
 import QuestionList from '../organism/QuestionList';
 import axios from 'axios';
 
 const QuestionLists = () => {
   const [Questions, setQuestions] = useState([]); // 백엔드 통신으로 데이터 받아오기
-  const [loading, setloading] = useState(false);
-  const [currentPage, setcurrentPage] = useState(1); // 초기 값 1
-  const [questionPerPage, setquestionPerPage] = useState(5); // 한 페이지에 보여줄 질문 개수
+  const [currentPage, setCurrentPage] = useState(1); // 초기 값 1
+  const [currentSize, setCurrentSize] = useState(5); // 한 페이지에 보여줄 질문 개수
 
   useEffect(() => {
     questionApi
@@ -19,18 +19,13 @@ const QuestionLists = () => {
       .catch((err) => console.log(err));
   }, []);
 
-  //Get Current Products
-  // const indexOfLastQuestion = Questions * questionPerPage;
-  // const indexOfFirstQuestion = indexOfLastQuestion - questionPerPage;
-  // const currentQuestion = Questions.slice(
-  //   indexOfFirstQuestion,
-  //   indexOfLastQuestion
-  // );
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
 
-  // const questionList = currentQuestion.map((question, index) => {
-  //   console.log(question);
-  //   return <QuestionList key={index} question={question} />;
-  // });
+  const handleSizeChange = (size) => {
+    setCurrentSize(size);
+  };
 
   return Questions.map((question, index) => {
     return <QuestionList key={index} question={question} />;
@@ -38,3 +33,24 @@ const QuestionLists = () => {
 };
 
 export default QuestionLists;
+
+const QuestionBox = styled.div`
+  width: 25px;
+  height: 27px;
+  background-color: transparent;
+  border-radius: 3px;
+  border: 1px solid var(--bc-medium);
+  color: var(--fc-medium);
+  padding: 0 var(--su8);
+  margin-left: var(--su2);
+  margin-right: var(--su2);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &:hover {
+    border-color: var(--bc-darker);
+    background-color: var(--black-100);
+    color: var(--fc-dark);
+  }
+`;
