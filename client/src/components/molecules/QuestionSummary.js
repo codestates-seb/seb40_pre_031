ece81646422e62c58ab5805a7cb5761853731343
dorrion <dhdms8696@naver.com>
2022-11-04 21:40:10 +0900

    🎨style : answers에 따른 조건부 렌더링

--- client/src/components/molecules/QuestionSummary.js
+++ client/src/components/molecules/QuestionSummary.js
@@ -3,9 +3,9 @@ import styled from 'styled-components';
 const QuestionSummary = ({ votes, answers, views }) => {
   return (
     <SummaryLayout>
-      <QuestionVotes>{votes} Votes</QuestionVotes>
-      <AnswerSummaryBox>{answers} answers</AnswerSummaryBox>
-      <QuestionViews>{views} views</QuestionViews>
+      <div>{votes} Votes</div>
+      <div className={answers ? 'green' : ' '}>{answers} answers</div>
+      <div>{views} views</div>
     </SummaryLayout>
   );
 };
@@ -19,30 +19,20 @@ const SummaryLayout = styled.div`
 
   width: 108px;
   height: 100px;
-
+  font-size: var(--fs-caption);
   margin-right: 16px;
 
   & div {
     margin-bottom: 4px;
+    color: var(--black-500);
   }
-`;
 
-const QuestionVotes = styled.div`
-  font-size: var(--fs-caption);
-`;
-
-const AnswerSummaryBox = styled.div`
-  width: 65px;
-  height: 25px;
-  border: 1px solid var(--green-700);
-  border-radius: 3px;
-
-  font-size: var(--fs-caption);
-  color: var(--green-700);
-  padding: 2px 4px;
-`;
-
-const QuestionViews = styled.div`
-  font-size: var(--fs-caption);
-  color: var(--black-500);
+  .green {
+    width: 65px;
+    height: 25px;
+    border: 1px solid var(--green-700);
+    border-radius: 3px;
+    color: var(--green-700);
+    padding: 2px 4px;
+  }
 `;
