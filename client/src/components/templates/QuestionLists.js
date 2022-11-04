aff880ff0c7a5104095f65d5e149a5830247fbd6
dorrion <dhdms8696@naver.com>
2022-11-04 22:20:36 +0900

    🔧refactor : 기능 분리

--- client/src/components/templates/QuestionLists.js
+++ client/src/components/templates/QuestionLists.js
@@ -7,8 +7,6 @@ import axios from 'axios';
 
 const QuestionLists = () => {
   const [Questions, setQuestions] = useState([]); // 백엔드 통신으로 데이터 받아오기
-  const [currentPage, setCurrentPage] = useState(1); // 초기 값 1
-  const [currentSize, setCurrentSize] = useState(5); // 한 페이지에 보여줄 질문 개수
 
   useEffect(() => {
     questionApi
@@ -19,38 +17,9 @@ const QuestionLists = () => {
       .catch((err) => console.log(err));
   }, []);
 
-  const handlePageChange = (page) => {
-    setCurrentPage(page);
-  };
-
-  const handleSizeChange = (size) => {
-    setCurrentSize(size);
-  };
-
   return Questions.map((question, index) => {
     return <QuestionList key={index} question={question} />;
   });
 };
 
 export default QuestionLists;
-
-const QuestionBox = styled.div`
-  width: 25px;
-  height: 27px;
-  background-color: transparent;
-  border-radius: 3px;
-  border: 1px solid var(--bc-medium);
-  color: var(--fc-medium);
-  padding: 0 var(--su8);
-  margin-left: var(--su2);
-  margin-right: var(--su2);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  &:hover {
-    border-color: var(--bc-darker);
-    background-color: var(--black-100);
-    color: var(--fc-dark);
-  }
-`;
