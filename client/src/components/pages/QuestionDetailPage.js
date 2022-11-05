e2807a4a6e80919ab539fe0733c8987356b59612
waymokorea <taw0207@naver.com>
2022-11-05 17:39:08 +0900

    ✨feat : 질문상세페이지 수정버튼

--- client/src/components/pages/QuestionDetailPage.js
+++ client/src/components/pages/QuestionDetailPage.js
@@ -57,11 +57,13 @@ const QuestionDetailPage = ({ question_id }) => {
                 ></QuestionDetail>
                 <QuestionComments
                   commentList={answer.commentList}
+                  questionid={question_id}
+                  answerid={answer.answerId}
                 ></QuestionComments>
               </>
             ))
           : null}
-          <PostAnswerBox></PostAnswerBox>
+        <PostAnswerBox questionid={question_id}></PostAnswerBox>
       </div>
     </QuestionDetailPage>
   ) : null;
