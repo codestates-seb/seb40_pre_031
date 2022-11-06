7573a69480cf6f343fa2c2c4d7b2da59e5f3316f
dorrion <dhdms8696@naver.com>
2022-11-07 01:43:23 +0900

    refactor: 주석 제거

--- client/src/components/organism/AskQuestion.js
+++ client/src/components/organism/AskQuestion.js
@@ -1,5 +1,5 @@
 import { useRef } from 'react';
-// import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import PostQuestionButton from '../atoms/PostQuestionButton';
 import AskQuestionEditorBox from '../molecules/AskQuestionEditorBox';
