6dc5eef6e24dcbcfadec51b53e2def45f312edd3
Bhinney <107696895+Bhinney@users.noreply.github.com>
2022-11-07 22:20:38 +0900

    refactor: authorization -> cookie (#250)

--- server/src/main/java/com/codestates/auth/filter/JwtVerificationFilter.java
+++ server/src/main/java/com/codestates/auth/filter/JwtVerificationFilter.java
@@ -50,13 +50,17 @@ public class JwtVerificationFilter extends OncePerRequestFilter {
 
 	@Override
 	protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
-		String authorization = request.getHeader("Authorization");
+		// String authorization = request.getHeader("Authorization");
+		String authorization = request.getHeader("Cookie");
 
-		return authorization == null || !authorization.startsWith("Bearer");
+		// return authorization == null || !authorization.startsWith("Bearer");
+		return authorization == null || !authorization.startsWith("Authorization");
+
 	}
 
 	private Map<String, Object> verifyJws(HttpServletRequest request) {
-		String jws = request.getHeader("Authorization").replace("Bearer", "");
+		// String jws = request.getHeader("Authorization").replace("Bearer", "");
+		String jws = request.getHeader("Cookie").replace("Authorization=Bearer", "");
 		String base64EncodedSecretKey = jwtTokenizer.encodeBase64SecretKey(jwtTokenizer.getSecretKey());
 		Map<String, Object> claims = jwtTokenizer.getClaims(jws, base64EncodedSecretKey).getBody();
 
