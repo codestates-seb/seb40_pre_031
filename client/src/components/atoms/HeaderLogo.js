117b68efd4333ef605305691b0555cdae4295200
dorrion <dhdms8696@naver.com>
2022-11-07 22:55:32 +0900

    refactor : 로고 클릭 시 이동 페이지
    - 로그인 여부에 따라

--- client/src/components/atoms/HeaderLogo.js
+++ client/src/components/atoms/HeaderLogo.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import { LogoIcon } from '../atoms/SvgHeaderIcons';
+import { useSelector } from 'react-redux';
 
 //Stackoverflow 로고 눌렀을 때, 메인페이지 "/main"로 연결
 const HeaderLogoBox = styled.div`
@@ -15,11 +16,19 @@ const HeaderLogoBox = styled.div`
 `;
 
 const HeaderLogo = () => {
+  const isLogin = useSelector((store) => store.authReducer.userStatus);
   return (
     <HeaderLogoBox>
-      <Link to="/main">
-        <LogoIcon />
-      </Link>
+      {isLogin ? (
+        <Link to="/">
+          <LogoIcon />
+        </Link>
+      ) : (
+        <Link to="/main">
+          {' '}
+          <LogoIcon />
+        </Link>
+      )}
     </HeaderLogoBox>
   );
 };
